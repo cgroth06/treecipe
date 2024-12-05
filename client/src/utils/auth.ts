3921616a1c4ffd0c888f60cce5b572087effed81
cgroth06 <christopher.m.groth@gmail.com>
2024-12-05 14:31:09 -0600

    auth.ts in client utils updated

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,7 +3,6 @@ import { type JwtPayload, jwtDecode } from 'jwt-decode';
 // Extending the JwtPayload interface to include additional data fields specific to the application.
 interface ExtendedJwt extends JwtPayload {
   data:{
-    username:string,
     email:string,
     id:string
   }
