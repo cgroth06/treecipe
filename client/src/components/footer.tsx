43736bf82dea2a91e5e6f7e221f436192fcbe7bb
EthanForrestCarr <ethan@ethancarr.com>
2024-12-06 14:50:34 -0600

    group effort

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from '../assets/CodexX Logo.png';
+import logo from '../assets/ArtVine_transparent.png';
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <img src={logo} alt="CodexX Logo" style={{ width: '100px', height: 'auto' }} />
+        <img src={logo} alt="ArtVine Logo" style={{ width: '100px', height: 'auto' }} />
         <p className="footer-text">
           Content copyright &copy; {new Date().getFullYear()} by CodexX. All rights reserved.
         </p>
