c1cd3289cc804b4e031b8beba84d9b2c83d63c83
EthanForrestCarr <ethan@ethancarr.com>
2024-12-05 20:43:48 -0600

    just changed some html

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -13,9 +13,9 @@ const Header = () => {
             <div className="container flex-row justify-space-between-lg justify-center align-center">
                 <div>
                     <Link className="text-light" to="/">
-                        <h1 className="m-0">Tech Thoughts</h1>
+                        <h1 className="m-0">Art Vine</h1>
                     </Link>
-                    <p className="m-0">Get into the mind of a programmer.</p>
+                    <p className="m-0">KABOOM!</p>
                 </div>
                 <div>
                     {/* Checking if the user is logged in to conditionally render profile link and logout button */}
@@ -34,6 +34,7 @@ const Header = () => {
                             <Link className="btn btn-lg btn-info m-2" to="/login">
                                 Login
                             </Link>
+                            <br />
                             <Link className="btn btn-lg btn-light m-2" to="/signup">
                                 Signup
                             </Link>
