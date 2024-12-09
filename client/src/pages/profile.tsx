d357dcad6841abc8cd128d08e4794b7d2518dcc2
EthanForrestCarr <ethan@ethancarr.com>
2024-12-08 20:33:36 -0600

    added follow button that should work eventually. Need to add more data to test.

--- client/src/pages/profile.tsx
+++ client/src/pages/profile.tsx
@@ -1,21 +1,39 @@
 import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { useQuery, useMutation } from '@apollo/client';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { FOLLOW_USER } from '../utils/mutations';
 
 const ProfilePage: React.FC = () => {
     const { name } = useParams<{ name: string }>();
     const { loading, error, data } = useQuery(QUERY_USER, {
         variables: { name },
     });
+    const [followUser] = useMutation(FOLLOW_USER, {
+        refetchQueries: [{ query: QUERY_ME }], // Refetch the current user's data to update follows
+    });
+
+    const handleFollow = async (followId: string) => {
+        try {
+            await followUser({ variables: { followId } });
+            alert('Followed successfully!');
+        } catch (err) {
+            console.error(err);
+            alert('Error following user.');
+        }
+    };
 
     if (loading) return <p>Loading profile...</p>;
     if (error) return <p>Error loading profile: {error.message}</p>;
 
     const user = data?.user;
+    const isFollowing = data?.me?.follows.some((f: any) => f._id === user._id);
 
     return (
         <div>
-            <h1>{user.name}</h1>
+            <h1>{user.name}'s Profile</h1>
+            <button onClick={() => handleFollow(user._id)} disabled={isFollowing}>
+                {isFollowing ? 'Following' : 'Follow'}
+            </button>
             <h2>Compositions</h2>
             <ul>
                 {user.compositions.map((composition: any) => (
@@ -30,3 +48,4 @@ const ProfilePage: React.FC = () => {
 };
 
 export default ProfilePage;
+
