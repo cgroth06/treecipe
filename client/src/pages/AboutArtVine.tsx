a21c2d2f00edda26aeb695136a96f6e5172409e4
DimintriLo <Dimintri.lo@gmail.com>
2024-12-11 12:47:31 -0600

    made changes to the css for AboutArtVine

--- client/src/pages/AboutArtVine.tsx
+++ client/src/pages/AboutArtVine.tsx
@@ -1,10 +1,11 @@
 import './AboutArtVine.css';
 import LogoImage from '../assets/ArtVine_transparent.png';
 
-const AboutUs = () => {
+const AboutArtVine = () => {
 
   return (
     <>
+
       <section className="hero is-small has-background-primary-dark">
         <div className="hero-body">
           <div className="columns">
@@ -12,7 +13,7 @@ const AboutUs = () => {
               <img className="is-pulled-right" src={LogoImage} height="120px" width="120px"></img>
             </div>
             <div className="column">
-              <p className="title has-text-primary">Welcome to ArtVine</p>
+              <p className="title has-text-primary">ArtVine</p>
               <p className="subtitle">Create and connect.</p>
             </div>
           </div>
@@ -21,32 +22,34 @@ const AboutUs = () => {
 
       <div className="container">
         <section>
-          <h2>Our Purpose</h2>
-          <p>At ArtVine, we aim to elevate the art of poetry by offering a platform where poets can share their work, engage with readers, and collaborate creatively.</p>
+          <h2 className='head1'>Our Purpose</h2>
+          <p className='head1-p'>At ArtVine, we aim to elevate the art of poetry by offering a platform where poets can share their work, engage with readers, and collaborate creatively.</p>
         </section>
 
-        <section>
-          <p className="title is-4">Key Features</p>
-          <div className="columns is-2">
-            <div className="box column">
-              <p className="title is-5">Share Your Voice</p>
-              <p>Upload and organize your poetry collections for a global audience. Share your words with the world and let your voice be heard.</p>
-            </div>
-            <div className="box column">
-              <p className="title is-5">Explore the Collection</p>
-              <p>Browse through a rich collection of poems from emerging and established poets, offering unique voices and perspectives.</p>
-            </div>
-            <div className="box column">
-              <p className="title is-5">Collaborate Creatively</p>
-              <p>Connect with other poets, readers, and artists to explore new creative opportunities and projects.</p>
-            </div>
+      <section>
+        <h2 className="title is-4 features">Key Features</h2>
+        <div className="columns is-2">
+          <div className="box column">
+            <h2 className="title is-5">Share Your Voice</h2>
+            <p>Upload and organize your poetry collections for a global audience. Share your words with the world and let your voice be heard.</p>
           </div>
-        </section>
+          <div className="box column">
+            <h2 className="title is-5">Explore the Collection</h2>
+            <p>Browse through a rich collection of poems from emerging and established poets, offering unique voices and perspectives.</p>
+          </div>
+          <div className="box column">
+            <h2 className="title is-5">Collaborate Creatively</h2>
+            <p>Collaborate creatively with a vibrant community. 
+              Engage deeply with poets, readers, and artists to explore exciting new creative projects and opportunities,
+              fostering a dynamic exchange of ideas and inspiration.</p>
+          </div>
 
-        <section>
-          <h2>Responsive & User-Friendly</h2>
-          <p>ArtVine is designed with you in mind, providing an easy and enjoyable experience for both poets and readers. Whether on desktop or mobile, enjoy the simplicity and beauty of the platform.</p>
-        </section>
+          <div className='box column'>
+            <h2 className="title is-5">Responsive & User-Friendly</h2>
+            <p >ArtVine is designed with you in mind, providing an easy and enjoyable experience for both poets and readers. Whether on desktop or mobile, enjoy the simplicity and beauty of the platform.</p>
+          </div>
+        </div>
+      </section>
 
         <section className="contact-section">
           <h2>Get in Touch</h2>
@@ -56,33 +59,26 @@ const AboutUs = () => {
         </section>
       </div>
 
-
-
-      <h3>Collaboration Members</h3>
+      <h3 className="collaboration-title">Collaboration Members</h3>
       <div className="collaboration">
 
         <div className="collaboration-member">
-          <h4>Brad Shurts</h4>
           <a href="https://github.com/modifiedyoke" className="contact-link">Brad Shurts</a>
         </div>
 
         <div className="collaboration-member">
-          <h4>Chris Groth</h4>
           <a href="https://github.com/cgroth06" className="contact-link">Chris Groth</a>
         </div>
 
         <div className="collaboration-member">
-          <h4>Dimintri Lo</h4>
           <a href="https://github.com/DimintriLo" className="contact-link">Dimintri Lo</a>
         </div>
 
         <div className="collaboration-member">
-          <h4>Ethan Carr</h4>
           <a href="https://github.com/EthanForrestCarr" className="contact-link">Ethan Carr</a>
         </div>
 
         <div className="collaboration-member">
-          <h4>Yahye Mohamed</h4>
           <a href="https://github.com/yahye-mohamed101" className="contact-link">Yahye Mohamed</a>
         </div>
       </div>
@@ -90,4 +86,4 @@ const AboutUs = () => {
   );
 }
 
-export default AboutUs;
+export default AboutArtVine;
